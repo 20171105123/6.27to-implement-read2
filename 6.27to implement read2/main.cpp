2b664990dd707a0cbf06a23d0f48669ebb26d823
20171105123 <s20171105123@admindeimac-34.local>
2018-06-27 10:47:52 +0800

    Initial Commit

---
+++ 6.27to implement read2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  6.27to implement read2
+//
+//  Created by 鲁祥 on 2018/6/27.
+//  Copyright © 2018年 鲁祥. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

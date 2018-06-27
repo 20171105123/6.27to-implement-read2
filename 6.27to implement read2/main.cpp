829fb08eb67a673aea7da9a9277c9a8e3aa526ad
20171105123 <s20171105123@admindeiMac-34.local>
2018-06-27 10:55:08 +0800

    to the implement read2

--- 6.27to implement read2/main.cpp
+++ 6.27to implement read2/main.cpp
@@ -1,15 +1,28 @@
 //
-//  main.cpp
-//  6.27to implement read2
+//  main.c
+//  6.27to implement read
 //
 //  Created by 鲁祥 on 2018/6/27.
 //  Copyright © 2018年 鲁祥. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a[10],s=0,max,min;
+    cout<<"请分别输入十个评委的分；"<<endl;
+    for (int i=0;i<10;i++)
+        cin>>a[i];
+    max=a[0];
+    min=a[9];
+    for(i=0;i<10;i++)
+    {
+        if(a[i]>max)max=a[i];
+        if(a[i]<min)min=a[i];
+        s+=a[i];
+    }
+    s=(s-max-min)/8;
+    cout<<"最后得分；"<<s<<endl;
     return 0;
 }
